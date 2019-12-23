932f75d3b6789777d7bb069bfbcf89d52cc2d592
m4reQ <51420351+m4reQ@users.noreply.github.com>
2019-12-23 17:53:11 +0100

    Updated settings
    Added new display setting "sdl_video_centered" and updated requirements. Added new diagnostics informations.

--- oss/diagtools/diagnostics.py
+++ oss/diagtools/diagnostics.py
@@ -1,33 +1,48 @@
-dont_check_net = False
-
 try:
     import os
     import sys
     import ctypes
     import platform
-except ImportError:
-    dont_check_net = True
+    import requests
+    import pygame
+except ImportError as e:
+    print(e)
 
 dir = os.getcwd()
-path = os.path.join(dir[:-10], 'osu!lool')
+path = os.path.join(dir[:-10], 'oss')
 sys.path.append(path)
 
-log = {"System_ver": "",
-       "Python_ver": "",
-       "Screen_res": "",
-       "CPU": "",
-       "path": ""}
+log = {"system_ver": '',
+       "python_ver": '',
+       "screen_res": '',
+       "CPU": '',
+       "path": '',
+       "pygame_ver": '',
+       "graphics_info": ''}
 
 def Get_sys_ver():
     name = os.name
     sys = platform.system()
     rel = platform.release()
-    
-    ver = name + ", " + sys + rel
-    return ver
+
+    return '{}, {} {}'.format(name, sys, rel)
+
+def Get_graph_info():
+    if platform.system() == 'Windows':
+        try:
+            import wmi
+
+            comp = wmi.WMI()
+            gpu_info = comp.Win32_VideoController()[0]
+            fgpu_info = 'Graphics card: {}'.format(gpu_info.Name)
+            return fgpu_info
+        except ImportError:
+            return '---'
+    else:
+        return '---'
 
 def Get_python_ver():
-    ver = ""
+    ver = ''
     for e in sys.version_info:
         ver += '.' + str(e)
     ver = ver[1:]
@@ -36,28 +51,30 @@ def Get_python_ver():
 def Get_screen_res():
     user32 = ctypes.windll.user32
     scrsize = user32.GetSystemMetrics(78), user32.GetSystemMetrics(79)
-    res = ('Width: ' + str(scrsize[0]) + ' Height: ' + str(scrsize[1]))
-    return str(res)
-
-def Get_cpu_inf():
-    return str(platform.processor())
-
-def Get_path():
-    return str(os.sys.path) + " " + str(os.popen('echo %path%').read())
+    res = 'Width: {} | Height: {}'.format(scrsize[0], scrsize[1])
+    return res
 
 def Gen_log():
     print('Generating log file...')
-    f = open('diaglog.txt', 'w+')
+
+    with open('diaglog.txt', 'w+') as  f:
+        f.write('')
     
-    log['System_ver'] = Get_sys_ver()
-    log['Python_ver'] = Get_python_ver()
-    log['path'] = Get_path()
-    log['Screen_res'] = Get_screen_res()
-    log['CPU'] = Get_cpu_inf()  
+    log['system_ver'] = Get_sys_ver()
+    log['python_ver'] = Get_python_ver()
+    log['path'] = '{} {}'.format(os.sys.path, os.popen('echo %path%').read())
+    log['screen_res'] = Get_screen_res()
+    log['CPU'] = str(platform.processor())
+    log['pygame_ver'] = str(pygame.version.ver)
+    try:
+        log['sdl_driver'] = os.environ['SDL_VIDEODRIVER']
+    except KeyError:
+        log['sdl_driver'] = 'not set'
+    log['graphics_info'] = Get_graph_info()
 
     for key, val in log.items():
-        f.write(str(key + ": " + val + "\n"))
-    f.close()
+        with open('diaglog.txt', 'a') as f:
+            f.write('{}: {}\n'.format(key, val))
 
 if __name__ == '__main__':
     Gen_log()
